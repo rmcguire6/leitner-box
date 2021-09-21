9aba08f1100142424193fe2ec939f252021fb883
Regina Mc Guire <28935544+rmcguire6@users.noreply.github.com>
2021-09-21 14:31:53 -0700

    feat: Add cards model

--- app/models.py
+++ app/models.py
@@ -3,6 +3,17 @@ from app import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
+    cards = db.relationship('Card', backref='creator', lazy='dynamic')
+    def __repr__(self):
+        return '<User {}'.format(self.username)
 
-def __repr__(self):
-    return '<User {}'.format(self.username)
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(64), required=True)
+    question = db.Column(db.String(64), required=True)
+    answer = db.Column(db.String(64), required=True)
+    active = db.Column(db.Boolean, default=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+    def __repr__(self):
+        return '<Card {}'.format(self.question)
