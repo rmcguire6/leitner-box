417184af780018c70a32e1c0d0d66b823594a9a8
Regina Mc Guire <28935544+rmcguire6@users.noreply.github.com>
2021-09-27 17:32:26 -0700

    feat: Add schemas for User and Card

--- app/models.py
+++ app/models.py
@@ -1,19 +1,35 @@
-from app import db
+from app import db, ma
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = "user"
+    user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    cards = db.relationship('Card', backref='creator', lazy='dynamic')
+    cards = db.relationship('Card', lazy='dynamic')
     def __repr__(self):
         return '<User {}'.format(self.username)
 
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('user_id', 'username', 'cards')
+
 class Card(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = "card"
+    card_id = db.Column(db.Integer, primary_key=True)
     subject = db.Column(db.String(64), nullable=False)
     question = db.Column(db.String(64), nullable=False)
     answer = db.Column(db.String(64), nullable=False)
     active = db.Column(db.Boolean, default=True)
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
     
     def __repr__(self):
         return '<Card {}'.format(self.question)
+
+class CardSchema(ma.Schema):
+    class Meta:
+        fields = ('card_id', 'subject', 'question', 'answer','active', 'user_id')
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+card_schema = CardSchema()
+cards_schema = CardSchema(many=True)
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,23 @@
 from app import app
-
+from app import models
+from flask import jsonify
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello World from Flask"
+
+@app.route('/users')
+def get_users():
+    users_list = models.User.query.all()
+    result = models.users_schema.dump(users_list[0])
+    return jsonify(result)    
+
+@app.route('/user/<int:user_id>', methods=["GET"])
+def get_user(user_id: int):
+    user = models.User.query.filter_by(user_id=user_id).first()
+    if user:
+        result = models.user_schema.dump(user)
+        return jsonify(result)
+    else:
+        return jsonify(message="That user does not exist"), 404
