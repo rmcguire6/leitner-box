5d7485597dc6dbba371adb42d4df1aca47a00c60
Regina McGuire <rmcguire@as.net>
2021-11-10 16:02:20 -0800

    bug: Remove unused migrations code

--- app/__init__.py
+++ app/__init__.py
@@ -3,16 +3,15 @@ import os
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from flask_migrate import Migrate
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'lebox.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
-migrate = Migrate(app, db)
 
 from app import routes
